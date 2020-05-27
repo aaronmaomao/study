9bc1c6ae6b707dd18a2a67bf83133671e57ebaae
mao <mao@DESKTOP-C75PGRL>
2020-05-27 09:21:27 +0800

    update

---
+++ aom.maozj.study.dsf/src/aom/maozj/study/dsf/launch/MLaunch.java
@@ -0,0 +1,14 @@
+package aom.maozj.study.dsf.launch;
+
+import org.eclipse.cdt.dsf.gdb.launching.GdbLaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ISourceLocator;
+
+public class MLaunch extends GdbLaunch{
+
+	public MLaunch(ILaunchConfiguration launchConfiguration, String mode, ISourceLocator locator) {
+		super(launchConfiguration, mode, locator);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- aom.maozj.study.dsf/src/aom/maozj/study/dsf/launch/MLaunchConfigurationDelegate.java
+++ aom.maozj.study.dsf/src/aom/maozj/study/dsf/launch/MLaunchConfigurationDelegate.java
@@ -1,12 +1,13 @@
 package aom.maozj.study.dsf.launch;
 
+import org.eclipse.cdt.dsf.gdb.launching.GdbLaunchDelegate;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate2;
 
-public class MLaunchConfigurationDelegate implements ILaunchConfigurationDelegate2 {
+public class MLaunchConfigurationDelegate extends GdbLaunchDelegate implements ILaunchConfigurationDelegate2 {
 
 	public MLaunchConfigurationDelegate() {
 		// TODO Auto-generated constructor stub
@@ -15,31 +16,13 @@ public class MLaunchConfigurationDelegate implements ILaunchConfigurationDelegat
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
 		// TODO Auto-generated method stub
-
+		super.launch(configuration, mode, launch, monitor);
 	}
 
 	@Override
 	public ILaunch getLaunch(ILaunchConfiguration configuration, String mode) throws CoreException {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	public boolean buildForLaunch(ILaunchConfiguration configuration, String mode, IProgressMonitor monitor) throws CoreException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean finalLaunchCheck(ILaunchConfiguration configuration, String mode, IProgressMonitor monitor) throws CoreException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean preLaunchCheck(ILaunchConfiguration configuration, String mode, IProgressMonitor monitor) throws CoreException {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
